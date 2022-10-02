7d8fd4e69e19746bbe4134ccccff5b1524d8e3f2
Yoda <im.yod4@gmail.com>
2022-10-02 23:50:40 +0330

    rename binary search

---
+++ luna/search/_binary_search.py
@@ -0,0 +1,14 @@
+# binary_search.py
+
+def binary_search(S, start, end, key):
+    mid = (start + end) // 2
+    if end < start:
+        return 'Not Found!'
+    if S[mid] == key:
+        return mid
+    else:
+        if key < S[mid]:
+            return binary_search(S, start, mid - 1, key)
+        else:
+            return binary_search(S, mid + 1, end, key)
+
