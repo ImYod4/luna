d457d5c19678d68f3154bf80184fbde99a9bcb64
Yoda <im.yod4@gmail.com>
2022-10-14 06:59:14 +0330

    change access level of methods and fix bugs

--- luna/tree/_linked_binary_tree.py
+++ luna/tree/_linked_binary_tree.py
@@ -18,7 +18,7 @@ class LinkedBinaryTree(BinaryTreeBase):
             return (type(self) is type(other)) and (self._node is other._node)
     def __init__(self):
         self._root = None
-        self._size = None
+        self._size = 0
     def _validate(self, p):
         if not isinstance(p, self.Position):
             raise TypeError('p is not proper position type')
@@ -26,7 +26,7 @@ class LinkedBinaryTree(BinaryTreeBase):
             raise ValueError('p does not belong to this container')
         if p._node is p._node._parent:
             raise ValueError('p is no longer valied')
-        return p._node._element
+        return p._node
     def __len__(self):
         return self._size
     def _make_position(self, node):
@@ -45,41 +45,39 @@ class LinkedBinaryTree(BinaryTreeBase):
             count += 1
         return count
     def root(self):
-        return self._make_position(self._root._node)
-    def positions(self):
-        pass
+        return self._make_position(None if not self._root else self._root._node)
     def left(self, p):
         node = self._validate(p)
         return self._make_position(node._left)
     def right(self, p):
         node = self._validate(p)
         return self._make_position(node._right)
-    def _add_root(self, e):
-        if self.root() is None:
+    def add_root(self, e):
+        if not self.root() is None:
             raise ValueError('Root exists')
         self._root = self._Node(e)
         self._size += 1
         return self._make_position(self._root)
-    def _add_left(self, p, e):
+    def add_left(self, p, e):
         node = self._validate(p)
         if not node._left is None:
             raise ValueError('Left node exists')
         node._left = self._Node(e, node)
         self._size += 1
         return self._make_position(node._left)
-    def _add_right(self, p, e):
+    def add_right(self, p, e):
         node = self._validate(p)
         if not node._right is None:
             raise ValueError('Right node exists')
         node._right = self._Node(e, node)
         self._size += 1
         return self._make_position(node._right)
-    def _replace(self, p, e):
+    def replace(self, p, e):
         node = self._validate(p)
         old_value = node._element
         node._element = e
         return old_value
-    def _delete(self, p):
+    def delete(self, p):
         node = self._validate(p)
         if self.num_childern(p) == 2:
             raise ValueError('p has two children')
@@ -114,3 +112,4 @@ class LinkedBinaryTree(BinaryTreeBase):
             node._right = T2._root
             T2._root = None
             T2._size = 0
+
