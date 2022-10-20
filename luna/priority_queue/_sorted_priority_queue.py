95cf2360d7284fc57a6bfd5069c3ba774247a6b1
Yoda <im.yod4@gmail.com>
2022-10-20 10:39:30 +0330

    add sorted priority queue

---
+++ luna/priority_queue/_sorted_priority_queue.py
@@ -0,0 +1,33 @@
+from luna.deque import PositionalList
+from ._priority_queue_base import PriorityQueueBase
+
+class Empty(Exception):
+    pass
+
+class SortedPriorityQueue(PriorityQueueBase):
+    def __init__(self):
+        self._data = PositionalList()
+    def _find_min(self):
+        if self.is_empty():
+            raise Empty('SortedPriorityQueue is empty')
+        return self._data.first()
+    def min(self):
+        p = self._find_min()
+        item = p.element()
+        return (item._ken, item._value)
+    def remove_min(self):
+        p = self._find_min()
+        item = p.element()
+        self._data.delete(p)
+        return (item._key, item._value)
+    def add(self, k, v):
+        new = self._Item(k, v)
+        walk = self._data.first()
+        while not (walk is None) and (new < walk):
+            walk = self._data.after(walk)
+        if walk is None:
+            self._data.add_first(new)
+        else:
+            self._data.add_after(walk, new)
+    def __len__(self):
+        return len(self._data)
