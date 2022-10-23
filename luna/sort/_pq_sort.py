6603597df1ac83faf229d47c2324fe71415e3809
Yoda <im.yod4@gmail.com>
2022-10-23 14:46:51 +0330

    add comments

--- luna/sort/_pq_sort.py
+++ luna/sort/_pq_sort.py
@@ -5,10 +5,13 @@ from ..priority_queue import HeapPriorityQueue
 def pq_sort(C, type_='h'):
     '''Sort a linked list based collection from luna module.'''
     if type_ == 'h':
+        # Heap Sort
         p = HeapPriorityQueue()
     elif type_ == 's':
+        # Selection Sort
         p = UnsortedPriorityQueue()
     elif type_ == 'i':
+        # Insertion Sort
         p = SortedPriorityQueue()
     else:
         raise TypeError('invalid sort type')
