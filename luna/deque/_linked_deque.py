4de3e88b64ec85f5b3533a3ac335fcbadf939c0d
Yoda <im.yod4@gmail.com>
2022-10-23 14:39:49 +0330

    rename methods insert_first and insert_last

--- luna/deque/_linked_deque.py
+++ luna/deque/_linked_deque.py
@@ -8,9 +8,9 @@ class LinkedDeque(_LinkedDequeBase):
         if self._is_empty():
             raise _Empty('deque is empty')
         return self._header._next._element
-    def insert_first(self, e):
+    def add_first(self, e):
         self._insert_between(e, self._header, self._header._next)
-    def insert_last(self, e):
+    def add_last(self, e):
         self._insert_between(e, self._trailer._prev, self._trailer)
     def delete_first(self):
         answer = self._delete_node(self._header._next)
