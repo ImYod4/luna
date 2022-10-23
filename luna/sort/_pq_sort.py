f001fa8a50778a6c9fde1ff7e12b496a9fe5c08a
Yoda <im.yod4@gmail.com>
2022-10-23 14:31:35 +0330

    implement sort algorithms with priority queue

---
+++ luna/sort/_pq_sort.py
@@ -0,0 +1,21 @@
+from ..priority_queue import SortedPriorityQueue
+from ..priority_queue import UnsortedPriorityQueue
+from ..priority_queue import HeapPriorityQueue
+
+def pq_sort(C, type_='h'):
+    '''Sort a linked list based collection from luna module.'''
+    if type_ == 'h':
+        p = HeapPriorityQueue()
+    elif type_ == 's':
+        p = UnsortedPriorityQueue()
+    elif type_ == 'i':
+        p = SortedPriorityQueue()
+    else:
+        raise TypeError('invalid sort type')
+    n = len(C)
+    for e in range(n):
+        element = C.delete(C.first())
+        p.add(element, None)
+    for e in range(n):
+        key, _ = p.reomve_min()
+        C.add_last(key)
