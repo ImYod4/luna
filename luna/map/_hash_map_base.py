edd5ba0d76cce9db19ee97ab4d5074e58c43a1fd
Yoda <im.yod4@gmail.com>
2022-11-05 16:32:53 +0330

    initial HashMapBase class

---
+++ luna/map/_hash_map_base.py
@@ -0,0 +1,13 @@
+from ._map_base import MapBase
+
+class HashMapBase(MapBase):
+    def __init__(self):
+        pass
+    def __getitem__(self, k):
+        pass
+    def __setitem__(self, k, v):
+        pass
+    def __delitem__(self, k):
+        pass
+    def _resize(self, c):
+        pass
